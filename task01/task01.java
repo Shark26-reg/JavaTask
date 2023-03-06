6fbdbeee8ecc7e509f662ca2adf4a0fbbb3f366c
Ivannikov Roman <shark71@inbox.ru>
2023-03-06 12:59:29 +0300

    first commit

---
+++ task01/task01.java
@@ -0,0 +1,17 @@
+
+
+package JavaTask.task01;
+
+/**
+ * 1.Вычислить n-ое треугольного число(сумма чисел от 1 до n), а так же n! (произведение чисел от 1 до n)
+Ввод:5
+Треугольное число 1 + 2 + 3 + 4 + 5 = 15
+n! 1 * 2 * 3 * 4 * 5 = 120
+ */
+public class task01 {
+    public static void main(String[] args) {
+        System.out.println();
+    }
+
+    
+}
